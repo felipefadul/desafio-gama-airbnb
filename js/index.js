301eb77927bf8cf4b7d23c6657639921e4ecffb8
Felipe Fadul <felipe.fadul@hotmail.com>
2020-05-10 16:57:23 -0300

    feat: Adiciona campo de Check-in e Check-out
    A partir da escolha das datas de Check-in e Check-out,
    é calculado o número de noites de hospedagem e
    atualizado o valor total da estadia.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-const apiUrl = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72"
+const apiUrl = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72";
 const cardsContainer = document.getElementById("cards-container");
 let data = [];
+let numberDays = 1;
 
 async function fetchCards() {
   let response = await fetch(apiUrl);
@@ -21,12 +22,18 @@ function renderCard(card) {
   <div class="card">
     <img src="${card.photo}" class="card-img-top card-image" alt="${card.name}">
     <div class="card-body">
-      <h4 class="card-title">${card.name}</h4>
-      <div id="information">
-        <div class="card-text text-muted">${card.property_type}</div>
-        <div class="card-text"><strong>R$ ${card.price}</strong>/noite</div>
-      </div>
+        <h4 class="card-title">${card.name}</h4>
+        <div id="information">
+          <div class="card-text text-muted">${card.property_type}</div>
+          <div class="card-text"><strong>R$ ${card.price}</strong>/noite</div>
+        </div>
     </div>
+    <hr>
+    <div id="total-price">
+      <div class="card-text mb-2">
+        Total p/ ${numberDays} ${numberDays > 1 ? 'noites' : 'noite'}: <strong>R$ ${card.price * numberDays},00</strong>
+      </div>
+    <div>
   </div>
   `;
   cardsContainer.appendChild(div);
@@ -34,6 +41,15 @@ function renderCard(card) {
 
 async function main() {
   data = await fetchCards();
+  flatpickr("#datepicker", {
+    mode: "range",
+    minDate: "today",
+    locale: "pt",
+    dateFormat: "d-m-Y",
+    onClose: function (selected) {
+      updateCardsTotal(daysBetween(selected[0], selected[1]));
+    },
+  });
   if (data[0])
     renderCards(data);
 }
@@ -51,4 +67,23 @@ function handleSearch() {
   });
 
   renderCards(filteredResults);
+}
+
+function daysBetween(date1, date2) {
+  const MILLISECONDS_IN_ONE_DAY = 1000 * 60 * 60 * 24;
+  let differenceInMilliseconds = Math.abs(date1 - date2);
+  
+  return Math.round(differenceInMilliseconds / MILLISECONDS_IN_ONE_DAY);
+}
+
+function updateCardsTotal(number) {
+  // If the number of days is equal, avoid re-rendering.
+  if (numberDays === number)
+    return;
+  
+  // If number is zero, update it to one.
+  number = number ? number : 1;
+  numberDays = number;
+  
+  handleSearch();
 }
